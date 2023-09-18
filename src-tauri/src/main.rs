499afb0917754a9cf0e9ba66f79cad516d05d87a
thompson <hi.thompson@hotmail.com>
2023-09-19 00:04:20 +0100

    norg-desktop backend mvp complete

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use norg_engine::agenda::AgendaDay;
+use norg_engine::agenda::{AgendaDay, AgendaType};
 use norg_engine::category::Category;
 use norg_engine::donna::generate_agenda;
 use norg_engine::helper::generate_category_list;
@@ -14,16 +14,18 @@ use std::collections::HashMap;
 //}
 
 #[derive(serde::Serialize)]
-struct Response {
-    categories: Vec<Category>,
-    agenda: Vec<AgendaDay>,
+struct AgendaDayResponse {
+    date: String,
+    items: Vec<String>,
 }
 
 #[tauri::command]
-fn get_path(path: &str) -> Response {
+fn generate_text(path: &str) -> Vec<AgendaDayResponse> {
     let file_list: Vec<String> = generate_category_list(path);
     let mut category_vec: Vec<Category> = Vec::new();
     let mut id_map: HashMap<u32, usize> = HashMap::new();
+    // category_vec[id_map.get(&(u32::try_from(6).unwrap())).unwrap().to_owned()].clone();
+    // would get category with id=6 which is in index 5 of the vector.
 
     for file in file_list {
         category_vec.push(generate_category_struct(file.as_str()));
@@ -32,20 +34,54 @@ fn get_path(path: &str) -> Response {
     for i in 0..category_vec.len() {
         id_map.insert(category_vec[i].id, i);
     }
-    // category_vec[id_map.get(&(u32::try_from(6).unwrap())).unwrap().to_owned()].clone();
-    // would get category with id=6 which is in index 5 of the vector.
-
     let agenda: Vec<AgendaDay> = generate_agenda(category_vec.to_owned());
 
-    Response {
-        categories: category_vec.to_owned(),
-        agenda,
+    let out: Vec<AgendaDayResponse> = generate_agenda_day_response(agenda);
+    out
+}
+
+fn generate_agenda_day_response(agenda: Vec<AgendaDay>) -> Vec<AgendaDayResponse> {
+    let mut agenda_day_response_vec: Vec<AgendaDayResponse> = Vec::new();
+
+    for agenda_day in agenda.to_owned().iter() {
+        let date_string = agenda_day.date.get_date_string();
+        let mut items_vec: Vec<String> = Vec::new();
+        match agenda_day.to_owned().agenda_items {
+            Some(_) => {
+                let agenda_items = agenda_day.to_owned().agenda_items.unwrap();
+                for agenda_item in agenda_items.iter() {
+                    let agenda_type = agenda_item.to_owned().agenda_type;
+                    match agenda_type {
+                        AgendaType::ScheduleItem(schedule_item) => {
+                            let start_time: String = schedule_item.date.start_time_string();
+                            let end_time: String = schedule_item.date.end_time_string();
+                            items_vec.push(format!("{}-{} (Scheduled) {}", start_time, end_time, schedule_item.name));
+                        }
+                        AgendaType::TodoItem(todo_item) => {
+                            let start_time: String = todo_item.date.start_time_string();
+                            let end_time: String = todo_item.date.end_time_string();
+                            items_vec.push(format!("{}-{} (Todo) {}", start_time, end_time, todo_item.title));
+                        }
+                    }
+                }
+
+            },
+            None => {
+                items_vec.push(String::from("None"));
+            }
+        }
+
+        agenda_day_response_vec.push(AgendaDayResponse{
+            date: date_string,
+            items: items_vec
+        })
     }
+    agenda_day_response_vec
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_path])
+        .invoke_handler(tauri::generate_handler![generate_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -3,37 +3,54 @@ import { open } from "@tauri-apps/api/dialog";
 
 //let greetInputEl: HTMLInputElement | null;
 //let greetMsgEl: HTMLElement | null;
+let testEl: HTMLElement | null;
 
-async function readFileContents() {
+let path: string | null;
+
+async function getPath() {
   //if (greetMsgEl && greetInputEl) {
   //  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   //  greetMsgEl.textContent = await invoke("greet", {
   //    name: greetInputEl.value,
   //  });
   //}
+
   try {
     const selectedPath = await open({
       directory: true,
       multiple: false,
       title: "Open Norg .yml File",
     });
-    await invoke("get_path", {
-      path: selectedPath,
-    })
-      .then((res) => {
-          console.debug(res);
-      })
-      .catch((e) => console.error(e));
+
+    if (typeof selectedPath === "string") {
+      path = selectedPath;
+    }
   } catch (err) {
     console.error(err);
+  }
+  generateText(); //TODO figure out how to put this in window event listener and run after async function getPath finishes
+}
+
+async function generateText() {
+  if (testEl && path) {
+    let agenda: Array<Object> = await invoke("generate_text", {
+      path,
+    });
+
+    console.debug(agenda);
+    //todo: loop through agenda and output...
+
+    let out: string = `test\n test`; //TODO figure out how to implement headers not just content
+    testEl.textContent = out;
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   //greetInputEl = document.querySelector("#greet-input");
   //greetMsgEl = document.querySelector("#greet-msg");
+  testEl = document.querySelector("#test");
   document.querySelector("#file-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    readFileContents();
+    getPath();
   });
 });
