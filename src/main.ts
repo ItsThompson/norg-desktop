5f7fef105ec38e2227311801c796bad0dfd93e8c
thompson <hi.thompson@hotmail.com>
2023-09-21 10:12:59 +0100

    AgendaDiv MVP complete

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,30 @@ import { open } from "@tauri-apps/api/dialog";
 
 //let greetInputEl: HTMLInputElement | null;
 //let greetMsgEl: HTMLElement | null;
-let testEl: HTMLElement | null;
+let agendaDiv: HTMLElement | null;
 
 let path: string | null;
 
-function generateHTML() {
-  const app = document.getElementById("test");
-  const p = document.createElement("p");
-  p.textContent = "Hello, World!";
-  app?.appendChild(p);
+function generateHTML(agenda: Array<Object>) {
+  //todo: loop through agenda and output...
+  for (let i = 0; i < agenda.length; i++) {
+    if (agenda[i].hasOwnProperty("date")) {
+      let dateString = (agenda[i] as any).date;
+      let dateh1 = document.createElement("h1");
+      dateh1.textContent = dateString;
+      agendaDiv?.appendChild(dateh1);
+    }
+    if (agenda[i].hasOwnProperty("items")) {
+        let ul = document.createElement("ul");
+        for (let j = 0; j < (agenda[i] as any).items.length; j++) {
+            let li = document.createElement("li");
+            let itemString = (agenda[i] as any).items[j];
+            li.textContent = itemString;
+            ul?.appendChild(li);
+        }
+        agendaDiv?.appendChild(ul);
+    }
+  }
 }
 
 async function getPath() {
@@ -39,24 +54,20 @@ async function getPath() {
 }
 
 async function generateText() {
-  if (testEl && path) {
+  if (agendaDiv && path) {
     let agenda: Array<Object> = await invoke("generate_text", {
       path,
     });
 
     console.debug(agenda);
-    //todo: loop through agenda and output...
-
-    let out: string = `test\n test`; //TODO figure out how to implement headers not just content
-    testEl.textContent = out;
-    generateHTML();
+    generateHTML(agenda);
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   //greetInputEl = document.querySelector("#greet-input");
   //greetMsgEl = document.querySelector("#greet-msg");
-  testEl = document.querySelector("#test");
+  agendaDiv = document.querySelector("#agenda");
   document.querySelector("#file-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     getPath();
